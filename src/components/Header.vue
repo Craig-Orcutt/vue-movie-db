f2c669548ae1c96ebf92820459a7155b742e8446
Craig Orcutt <craigaorcutt@gmail.com>
2018-07-03 13:55:42 -0500

    minor tweaks to headers

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,25 +1,36 @@
 <template>
   <header>
-    <h1>{{title}}</h1>
+    <h1>{{ newTitle }}</h1>
   </header>
 </template>
 
 <script>
 export default {
-  name: 'Header',
+  name: "Header",
   props: {
     title: String
+  },
+  data() {
+    return {
+      name: 'Craig'
+    }
+  },
+  computed: {
+    newTitle: function() {
+      console.log('this', this);
+      return `${this.name} Orcutt `
+    }
   }
-}
+};
 </script>
 
 <style scoped>
-  header {
-    background-color: #111;
-    padding: 1.5em;
-    color: white
-  }
-  h1 {
-    margin: 0
-  }
+header {
+  background-color: #111;
+  padding: 1.5em;
+  color: white;
+}
+h1 {
+  margin: 0;
+}
 </style>
