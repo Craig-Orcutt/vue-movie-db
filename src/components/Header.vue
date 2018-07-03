aaf0589eb741608f632d86cb842b25f63a0e7047
Craig Orcutt <craigaorcutt@gmail.com>
2018-07-02 21:42:22 -0500

    added header component

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <h1>{{title}}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    title: String
+  }
+}
+</script>
+
+<style scoped>
+  header {
+    background-color: #111;
+    padding: 1.5em;
+    color: white
+  }
+  h1 {
+    margin: 0
+  }
+</style>
