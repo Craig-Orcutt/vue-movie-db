5d3f2ec2b35c7c985ad39624ea22a358e05391f8
Craig Orcutt <craigaorcutt@gmail.com>
2018-07-03 13:57:05 -0500

    created component list of movies. testing out with api calls to movie db

---
+++ src/components/MoviesList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    {{movie.title}}
+  </div>
+</template>
+
+<script>
+import {apiKey} from '../assets/apiKey'
+
+export default {
+  name: "MoviesList",
+
+  data () {
+    return {
+      movie: {},
+
+    }
+  },
+
+  created: function () {
+    this.fetchData()
+  },
+
+
+  methods: {
+    fetchData: async function() {
+      try {
+        const res = await fetch(
+          `https://api.themoviedb.org/3/movie/550?api_key=${apiKey}`
+        );
+        const movie  = await res.json()
+        this.movie = movie
+      } catch (e) {
+        console.log("error", e);
+      }
+    }
+  }
+
+
+};
+</script>
+
+<style>
+</style>
