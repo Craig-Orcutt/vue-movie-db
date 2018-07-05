df2cb7a94160186e4f4ce2244f6b7f37bc7c0c54
Craig Orcutt <craigaorcutt@gmail.com>
2018-07-05 11:34:42 -0500

    removed components from app. now routing through router

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,23 @@
 <template>
-  <div id="app" v-if="status === 'Ready'">
+  <div id="app" >
     <Header :title='title'      />
-    <MoviesList />
+    <router-view />
   </div>
-  <div v-else-if="status === 'Loading' ">
-    Loading...
-  </div>
-  <div v-else>
-    Error...
-  </div>
+
 </template>
 
 <script>
 import Header from "./components/Header.vue";
-import MoviesList from './components/MoviesList'
 
 export default {
   name: "app",
   data() {
     return {
-      hello: "Hello World",
-      title: "Vue Movie DB",
-      isTrue: true,
-      status: 'Ready'
+      title: "Vue Movie DB"
     };
   },
   components: {
-    Header,
-    MoviesList
+    Header
   }
 };
 </script>
@@ -38,7 +28,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: white;
 }
 </style>
