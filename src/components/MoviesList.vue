03714522281fdf1aa4fe020d706265d6062d4c65
Craig Orcutt <craigaorcutt@gmail.com>
2018-07-05 11:36:43 -0500

    minor style changes

--- src/components/MoviesList.vue
+++ src/components/MoviesList.vue
@@ -1,44 +1,55 @@
 <template>
-  <div>
-    {{movie.title}}
-  </div>
+  <ul>
+    <li v-for='movie in movies'>
+      <Movie :movie='movie' />
+    </li>
+  </ul>
 </template>
 
 <script>
-import {apiKey} from '../assets/apiKey'
+import { apiKey } from "../assets/apiKey";
+import Movie from "./Movie.vue";
 
 export default {
   name: "MoviesList",
 
-  data () {
+  data() {
     return {
-      movie: {},
-
-    }
+      movies: []
+    };
   },
 
-  created: function () {
-    this.fetchData()
+  created: function() {
+    this.fetchData();
   },
-
 
   methods: {
     fetchData: async function() {
       try {
         const res = await fetch(
-          `https://api.themoviedb.org/3/movie/550?api_key=${apiKey}`
+          `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${apiKey}`
         );
-        const movie  = await res.json()
-        this.movie = movie
+        const movies = await res.json();
+        this.movies = movies.results;
       } catch (e) {
         console.log("error", e);
       }
     }
+  },
+  components: {
+    Movie
   }
-
-
 };
 </script>
 
-<style>
+<style scoped>
+ul {
+  display: grid;
+  list-style: none;
+  padding: 1rem;
+  margin: 0;
+  grid-row-gap: 1rem;
+  grid-template-columns: repeat(6, 1fr);
+}
 </style>
+
